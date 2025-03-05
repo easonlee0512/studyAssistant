b7236ec1bae78ae87b1d92881276e11801fd9726
easonlee0512 <liyichen0512@gmail.com>
2025-03-05 20:49:02 +0800

    test

--- studyAssistant/ContentView.swift
+++ studyAssistant/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("HI")
+            Text("eason")
+            
         }
         .padding()
     }
