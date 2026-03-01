5394d13f29b0d39c78ebce5cdd70b4f8663df7e1
bruce <a0909861061@gmail.com>
2026-03-02 02:33:02 +0800

    Remove unnecessary user-specific files and sensitive configuration

--- studyAssistant/Config.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-enum Config {
-    static let openAIKey = "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"
-}
